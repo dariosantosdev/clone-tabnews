d93bf60830ec37b26b6f7b135924aa212254a9c3
Dario Santos <kkdario@gmail.com>
2024-03-18 05:40:01 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return<h1>Tamires, eu te Amo!!! Se vc me ama, dá uma risadinha!kkk </h1>
-    }
+  return <h1>Tamires, eu te Amo!!! Se vc me ama, dá uma risadinha!kkk </h1>;
+}
 
-    export default Home;
+export default Home;
